71c6032862902d5f132cb3d9dc23c80e7c352864
Jason <>
2018-08-07 21:37:48 -0400

    Created Spring Boot

---
+++ src/main/java/org/apache/maven/springboot/MovieDatabaseApp.java
@@ -0,0 +1,15 @@
+package org.apache.maven.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MovieDatabaseApp.class, args);
+
+	}
+
+}
