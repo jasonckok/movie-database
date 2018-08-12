ed59ab10e9151db6316911a90876e7bab1d636e1
Gavin Baker <gavnbaker@live.com>
2018-08-12 02:30:47 -0400

    Added movie list component to main app componet. Added app css file

---
+++ frontend/movie-database-client/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import MovieList from './components/movie-list/Movie-list';
+import './App.css';
+
+const movies = [
+  {
+      id: 1,
+      name: 'Ghost-Stories',
+      releaseDate: '2017',
+      photo: 'http://via.placeholder.com/200',
+  },
+  {
+      id: 2,
+      name: 'Shrek',
+      releaseDate: '2011',
+      photo: 'http://via.placeholder.com/200',
+  },
+  {
+      id: 3,
+      name: 'Inception',
+      releaseDate: '2013',
+      photo: 'http://via.placeholder.com/200',
+  },
+  {
+      id: 4,
+      name: 'I-Robot',
+      releaseDate: '2005',
+      photo: 'http://via.placeholder.com/200',
+  },
+  {
+      id: 5,
+      name: 'Pan\'s Labrith',
+      releaseDate: '2007',
+      photo: 'http://via.placeholder.com/200',
+  },
+  {
+      id: 6,
+      name: 'Interstellar',
+      releaseDate: '2011',
+      photo: 'http://via.placeholder.com/200',
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: movies,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1 className='heading'>Welcome to Movie Database</h1>
+        <CardList movies={this.state.movies} />
+      </div>
+     
+    );
+  }
+}
+
+export default App;
