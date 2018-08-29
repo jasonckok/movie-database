39261910e1f3149e7e69f1f41fff9fb3e339357e
Jason Kok <17363601+jasonckok@users.noreply.github.com>
2018-08-28 20:20:26 -0400

    Lint code

--- backend/src/main/java/org/apache/maven/springboot/MovieDatabaseApp.java
+++ backend/src/main/java/org/apache/maven/springboot/MovieDatabaseApp.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieDatabaseApp {
-
+	
 	public static void main(String[] args) {
-		
 		SpringApplication.run(MovieDatabaseApp.class, args);
-
 	}
-
 }
--- backend/src/main/java/org/apache/maven/springboot/movie/MovieService.java
+++ backend/src/main/java/org/apache/maven/springboot/movie/MovieService.java
@@ -46,7 +46,6 @@ public class MovieService {
 				return;
 			}
 		}
-		
 	}
 
 	// Delete a movie
